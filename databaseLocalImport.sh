b4fa0b5660fe535ceb0bd3de8174a2af3122b8fa
deirde <davidelongo79@gmail.com>
2018-08-18 07:09:15 -0400

    Update database import procedure.

--- databaseLocalImport.sh
+++ databaseLocalImport.sh
@@ -17,6 +17,10 @@ if [ "$REPLY" == "Y" ]; then
     if [ ! -f import-me.sql ]; then
         echo "The file <import-me.sql> doesn't exist! I can't proceed further."
     else
+        sed -i 's/utf8mb4_unicode_520_ci/utf8_general_ci/g' import-me.sql
+        sed -i 's/utf8_general_ci_unicode_ci/utf8_general_ci/g' import-me.sql
+        sed -i 's/utf8_general_ci/utf8_general_ci/g' import-me.sql
+        sed -i 's/utf8mb4/utf8/g' import-me.sql
         mysqldump -u${DB_LC_UID} -p${DB_LC_PSW} -h ${DB_LC_HOST} --add-drop-table --no-data ${DB_LC_NAME} | grep ^DROP | mysql -u${DB_LC_UID} -p${DB_LC_PSW} -h ${DB_LC_HOST} ${DB_LC_NAME}
         mysql -u $DB_LC_UID -p$DB_LC_PSW $DB_LC_NAME < import-me.sql
         rm -f import-me.sql
